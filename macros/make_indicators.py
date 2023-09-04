e8d686b5861837969879b1bb58a0a75bed18966a
datnguye <datnguyen.it09@gmail.com>
2023-09-04 11:10:57 +0700

    Add test model

---
+++ macros/make_indicators.py
@@ -0,0 +1,10 @@
+from sqlmesh import macro
+
+@macro()
+def make_indicators(evaluator, string_column, string_values):
+  cases = []
+
+  for value in string_values:
+    cases.append(f"CASE WHEN {string_column} = '{value}' THEN '{value}' ELSE NULL END AS {string_column}_{value}")
+
+  return cases
